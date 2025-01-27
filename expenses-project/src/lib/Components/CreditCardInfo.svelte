ff76663a944665d412cc909d220c9d930884b821
Tim Vorwald <timothyvorwald@gmail.com>
2025-01-26 20:44:31 -0600

    started creation of credit card page

---
+++ expenses-project/src/lib/Components/CreditCardInfo.svelte
@@ -0,0 +1,32 @@
+<script>
+import NavBar from "./NavBar.svelte";
+import SearchBar from "./SearchBar.svelte";
+</script>
+
+<div>
+    <NavBar/>
+    <SearchBar/>
+</div>
+
+<div>
+    <h1>Credit Card Information</h1>
+    <br>
+    <h2>*In the instance where a receipt number is not present the receipt number will default to the transaction number
+    and vice versa</h2>
+    <table>
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Date</th>
+            <th>Location of Purchase</th>
+            <th>Purchase Amount</th>
+            <th>Transaction Number</th>
+            <th>Receipt Number</th>
+        </tr>
+        </thead>
+    </table>
+</div>
+
+<style>
+    
+</style>
