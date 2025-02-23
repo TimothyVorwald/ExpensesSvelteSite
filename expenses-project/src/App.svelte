59fc371a239bd73fe850b46953244c79b7560fcf
Tim Vorwald <timothyvorwald@gmail.com>
2025-02-22 19:46:26 -0600

    Extracted out the welcome screen details into a separate component and added that component to the current view check

--- expenses-project/src/App.svelte
+++ expenses-project/src/App.svelte
@@ -4,33 +4,20 @@
   import navigationPage from "./util/navigationPage";
   import CreditCardInfo from "./lib/Components/CreditCardInfo.svelte";
   import BankInfo from "./lib/Components/BankInfo.svelte";
+  import Home from "./lib/Components/Home.svelte";
   
   let currentView = navigationPage.home;
 </script>
 
-<div>
   <NavBar bind:currentView on:click="{() => currentView}"/>
   <SearchBar/>
-  
-  <h1>Welcome!</h1>
-  <p>The purpose of this app is to keep track of payments for various expenses. 
-  These include payments from credit cards, and contains bank information such as checking
-  and savings account details. The information entered and displayed for credit card details
-  includes: the date of purchase, the location of the purchase, the purchase amount, the transaction/receipt
-  number, and will also the total amount spent with the card. The checking and saving details will include a
-  date, the account number, and the account balance.</p>
-  <br>
-  <br>
-  <h1>Enjoy using this app made with Svelte!!</h1>
-  <br>
-  <br>
-  <h1>Thank You!!</h1>
-</div>
 
 {#if currentView === navigationPage.creditcard}
   <CreditCardInfo/>
 {:else if currentView === navigationPage.bank}
   <BankInfo/>
+{:else}
+  <Home/>
 {/if}
 
 <style>
---
+++ expenses-project/src/lib/Components/Home.svelte
@@ -0,0 +1,23 @@
+<script>
+    
+</script>
+
+<div>
+<h1>Welcome!</h1>
+<p>The purpose of this app is to keep track of payments for various expenses.
+    These include payments from credit cards, and contains bank information such as checking
+    and savings account details. The information entered and displayed for credit card details
+    includes: the date of purchase, the location of the purchase, the purchase amount, the transaction/receipt
+    number, and will also the total amount spent with the card. The checking and saving details will include a
+    date, the account number, and the account balance.</p>
+<br>
+<br>
+<h1>Enjoy using this app made with Svelte!!</h1>
+<br>
+<br>
+<h1>Thank You!!</h1>
+</div>
+
+<style>
+    
+</style>
