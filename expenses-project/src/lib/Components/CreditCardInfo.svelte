0d28ff9c7131153ca3a0a7896fc1a37ce45f0718
timothyvorwald <timothyvorwald@gmail.com>
2025-05-25 09:24:27 -0500

    Added in test info into the credit card table, added progress bar, added function for calculating percent spent, started work on making progress bar dynamic

--- expenses-project/src/lib/Components/CreditCardInfo.svelte
+++ expenses-project/src/lib/Components/CreditCardInfo.svelte
@@ -1,5 +1,20 @@
 <script>
 import {Table} from "@sveltestrap/sveltestrap";
+import {onMount} from "svelte";
+
+const creditLimit = 1000;
+let currentAmountSpent = 800;
+let percentageSpent = 0.0;
+
+function PercentageOfCreditLimitSpent()
+{
+    percentageSpent = (currentAmountSpent/creditLimit) * 100;
+}
+
+onMount(async () => {
+    PercentageOfCreditLimitSpent();
+});
+
 </script>
 
 <div>
@@ -7,6 +22,13 @@ import {Table} from "@sveltestrap/sveltestrap";
     <br>
     <h2>*In the instance where a receipt number is not present the receipt number will default to the transaction number
         and vice versa</h2>
+    <br>
+    {#if percentageSpent >= 75.0}
+        <h1 class="text-warning">WARNING: You are nearing your credit limit for this month!</h1>
+    {/if}
+    <div class="progress">
+        <div class="progress-bar" role="progressbar" style="width: {percentageSpent}%" aria-valuenow="{percentageSpent}%" aria-valuemin="0" aria-valuemax="100">{percentageSpent}%</div>
+    </div>
     <Table class="ccTable">
         <thead>
         <tr>
@@ -17,11 +39,27 @@ import {Table} from "@sveltestrap/sveltestrap";
             <th>Transaction Number</th>
             <th>Receipt Number</th>
         </tr>
+        <tr>
+            <td>Timothy Vorwald</td>
+            <td>05/25/2025</td>
+            <td>Fareway</td>
+            <td>100.00</td>
+            <td>1</td>
+            <td>1</td>
+        </tr>
     </Table>
 </div>
 
 <style>
     :global(.ccTable){
         width: -webkit-fill-available;
+    }
+    .progress-bar{
+        color: red;
+        background-color: blue;
+        color-scheme: dark;
+    }
+    .text-warning{
+        color: red;
     }
 </style>
